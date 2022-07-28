24508e4639b0f3ccfeed54193363646ef0f946fa
Abd-elsalam <abdelsalm@gmail.com>
2022-07-28 15:35:53 +0200

    week1 and week2

---
+++ session/js/fun.js
@@ -0,0 +1,69 @@
+// // // Coding Challenge one
+// let massMark=78;
+// let heightMark=1.69;
+// let massJhane=92;
+// let heightJhone=1.95;
+// let bmiMark=(massMark/heightMark)*(heightMark);
+// let bimJhone=(massJhane/heightJhone)*(heightJhone);
+// console.log(bmiMark +" "+ bimJhone);
+// let markHigherBMI = bmiMark >bimJhone;
+// console.log(markHigherBMI);
+// //////////////////////////////////////////
+// //Coding Challenge tow
+// if(bmiMark>bimJhone){
+
+// console.log("Mark's BMI is higher than John's");
+
+
+// }else{
+
+// console.log("John's BMI is higher than Mark's!");
+
+
+// }
+// if(bmiMark>bimJhone){
+
+//     console.log(`Mark's BMI ${bmiMark} is higher than John's`);
+    
+    
+//     }else{
+    
+// console.log(`John's BMI ${bimJhone}   is higher than Mark's!`);
+    
+    
+//     }
+
+//Coding Challenge three
+
+let  DolphinsAverag =(96+108+89)/3;
+let KoalasAverag =(88+91+110)/3;
+console.log(DolphinsAverag);
+console.log(KoalasAverag);
+
+if(DolphinsAverag>KoalasAverag){
+
+console.log("Dolphins team is  the winner");
+
+
+}else if(KoalasAverag>DolphinsAverag){
+
+    console.log("Koalas team is  the winner");
+
+}else{
+
+console.log("draw ");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
